6df7d8e8481efb593df9311e5919fa55ef26fc14
And200 <nosredna2002@gmail.com>
2023-01-13 20:04:02 -0500

    changed config enabled

--- microservices-conf/build.gradle.kts
+++ microservices-conf/build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
 	// https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-bootstrap
-	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.0")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
