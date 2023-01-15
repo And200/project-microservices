3f5982d6dbdf8c5a05b2639fa7b12e5c309b37e6
And200 <nosredna2002@gmail.com>
2023-01-15 14:47:27 -0500

    added registry-service

---
+++ registry-service/src/test/java/com/edu/cun/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cun.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
