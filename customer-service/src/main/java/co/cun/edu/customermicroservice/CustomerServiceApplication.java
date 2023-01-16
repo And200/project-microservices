36eddb6f1e4691555bc56b508fe1512905af7dba
And200 <nosredna2002@gmail.com>
2023-01-16 13:02:33 -0500

    customer Microservices config

--- customer-service/src/main/java/co/cun/edu/customermicroservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/co/cun/edu/customermicroservice/CustomerServiceApplication.java
@@ -1,11 +1,12 @@
 package co.cun.edu.customermicroservice;
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
 @EnableEurekaClient
 @SpringBootApplication
-@EnableEurekaServer		
 public class CustomerServiceApplication {
 
 
